ce3d0bc12232d321167dfc6f1ba32e4c705a5624
e-koch <koch.eric.w@gmail.com>
2016-03-04 10:59:36 -0700

    Move azimuthal_profiles

--- basics/profile.py
+++ basics/profile.py
@@ -150,3 +150,47 @@ def _line_profile_coordinates(src, dst, linewidth=1):
     perp_cols = np.array([np.linspace(col_i - col_width, col_i + col_width,
                                       linewidth) for col_i in line_col])
     return np.array([perp_rows, perp_cols])
+
+
+def azimuthal_profiles(image, blob, ntheta=360, verbose=False,
+                       extend_factor=1.0):
+    '''
+    Calculate azimuthal profiles from the centre of a bubble to its edge.
+    '''
+
+    y0, x0, a, b, pa = blob.copy()
+
+    a *= extend_factor
+    b *= extend_factor
+
+    thetas = np.linspace(0.0, 2*np.pi, ntheta)
+
+    profiles = []
+
+    for i, theta in enumerate(thetas):
+
+        end_pt = (y0 + a*np.cos(theta)*np.sin(pa) - b*np.sin(theta)*np.cos(pa),
+                  x0 + a*np.cos(theta)*np.cos(pa) - b*np.sin(theta)*np.sin(pa))
+
+        profile, dists = profile_line(image, (y0, x0), end_pt)
+
+        profiles.append(profile)
+
+        if verbose:
+            import matplotlib.pyplot as p
+            import time
+
+            p.subplot(121)
+            p.imshow(image, cmap='afmhot')
+            p.plot(x0, y0, 'bD')
+            p.plot(end_pt[1], end_pt[0], 'rD')
+            p.xlim([0, image.shape[1]])
+            p.ylim([0, image.shape[0]])
+
+            p.subplot(122)
+            p.plot(dists, profile, 'bD-')
+
+            p.draw()
+            time.sleep(0.05)
+
+    return profiles

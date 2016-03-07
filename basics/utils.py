0fea943b8752de737604d4cb1e36eaf539c7ffb1
e-koch <koch.eric.w@gmail.com>
2016-03-07 10:46:28 -0700

    Add docstring and link to what the function is based on

--- basics/utils.py
+++ basics/utils.py
@@ -8,6 +8,25 @@ def arctan_transform(array, thresh):
 
 
 def dist_uppertri(cond_arr, shape):
+    '''
+    Convert a condensed distance matrix into the upper triangle. This is what
+    squareform in scipy.spatial.distance does, without filling the lower
+    triangle.
+
+    From: http://stackoverflow.com/questions/13079563/how-does-condensed-distance-matrix-work-pdist
+
+    Parameters
+    ----------
+    cond_arr : np.ndarray
+        Condensed distance matrix from pdist.
+    shape : int
+        Number of entries in the data used to calculate the distance matrix.
+
+    Returns
+    -------
+    dist_arr : np.ndarray
+        Upper triangular distance matrix.
+    '''
     dist_arr = np.zeros((shape, ) * 2, dtype=cond_arr.dtype)
 
     def unrav_ind(i, j, n):

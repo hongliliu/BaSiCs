f8b3f8dc73bb49da8ff24fb990679c60398943bb
e-koch <koch.eric.w@gmail.com>
2016-03-07 14:20:42 -0700

    Add clustering on channels

--- basics/clustering.py
+++ basics/clustering.py
@@ -4,6 +4,7 @@ from scipy.spatial.distance import pdist, squareform
 from scipy.cluster.hierarchy import fcluster, fclusterdata, linkage
 
 from log import overlap_metric
+from utils import dist_uppertri
 
 
 def cluster_2D_regions(twod_region_props, metric='position', cut_val=18):
@@ -11,13 +12,13 @@ def cluster_2D_regions(twod_region_props, metric='position', cut_val=18):
     Cluster 2D Bubble regions by their postion or overlap.
     '''
 
+    # Cluster on the x, y centre positions
     if metric is "position":
 
         cluster_idx = fclusterdata(twod_region_props[:, 1:3], cut_val,
                                    criterion='distance', method='complete')
 
-        return cluster_idx
-
+    # Cluster on the spatial overlap of regions
     elif metric is "overlap":
 
         if cut_val > 1.0:
@@ -28,7 +29,16 @@ def cluster_2D_regions(twod_region_props, metric='position', cut_val=18):
         link_mat = linkage(1 - sims, 'complete')
         cluster_idx = fcluster(link_mat, cut_val, criterion='distance')
 
-        return cluster_idx
+    # Cluster on the connectivity of regions in the spectral dimension
+    elif metric is 'channel':
+
+        Warning("cut_val forced to 1.0 for channel clustering.")
+
+        cluster_idx = fclusterdata(twod_region_props[:, :1], 1.0,
+                                   criterion='distance', method='single',
+                                   metric='cityblock')
 
     else:
         raise ValueError("metric must be 'position' or 'overlap'.")
+
+    return cluster_idx

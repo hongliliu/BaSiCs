3ab515c0a68b40d5f226670eb42f6fddffb71902
e-koch <koch.eric.w@gmail.com>
2016-03-10 16:42:34 -0700

    Update 2D testing script

--- test_wavelet_decomp.py
+++ test_wavelet_decomp.py
@@ -23,10 +23,10 @@ cube = cube[38:65, 500:1500, 500:1500]
 # Find sigma in an empty channel
 sigma = sig_clip(cube[0].value, nsig=10)
 
-bubble_15 = BubbleFinder2D(cube[15])
+bubble_15 = BubbleFinder2D(cube[15], channel=15)
 # bubble_15.apply_bilateral_filter()
-bubble_15.multiscale_bubblefind(sigma=sigma, overlap_frac=0.7)
-# bubble_15.region_rejection(value_thresh=3*sigma)
+bubble_15.multiscale_bubblefind(sigma=sigma, overlap_frac=0.5)
+bubble_15.region_rejection(value_thresh=3*sigma)
 # bubble_15.apply_atan_transform(np.percentile(bubble_15.array, 90))
 
 cols = ['b', 'g', 'c', 'm', 'r', 'k']
@@ -37,19 +37,13 @@ ax = p.subplot(111)
 
 ax.imshow(bubble_15.array, cmap='afmhot')
 
-for blob in bubble_15.region_params:
-    y, x, rmaj, rmin, pa = blob
-    c = Ellipse((x, y), width=2*rmaj, height=2*rmin, angle=np.rad2deg(pa),
-                color='b', fill=False, linewidth=2)
-    # c = p.Circle((x, y), r, color='g', fill=False, linewidth=2)
-    ax.add_patch(c)
-    ax.plot(x, y, 'bD')
-
-p.xlim([0, bubble_15.array.shape[1]])
-p.ylim([0, bubble_15.array.shape[0]])
-
-for bub in bubble_15.bubble2D_candidates:
-    p.contour(bub.find_shape(cube[15].value, nsig_thresh=0.,
+for bub in bubble_15.regions:
+    ax.add_patch(bub.as_patch(color='b', fill=False, linewidth=2))
+    ax.plot(bub.x, bub.y, 'bD')
+    p.contour(bub.find_shape(cube[15].value, nsig_thresh=1.,
                              return_array='full',
                              value_thresh=3*sigma,
                              linewidth=3), colors='g')
+
+p.xlim([0, bubble_15.array.shape[1]])
+p.ylim([0, bubble_15.array.shape[0]])

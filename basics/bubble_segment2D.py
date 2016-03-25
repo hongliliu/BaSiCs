Wrong sign for parametric ellipse equation

basics/bubble_segment2D.py
@@ -3,12 +3,15 @@ import numpy as np
 import warnings
 from astropy.nddata.utils import extract_array, add_array
 import astropy.units as u
+from skimage.measure import EllipseModel, CircleModel, ransac
+from cv2 import fitEllipse
 
 from spectral_cube.lower_dimensional_structures import LowerDimensionalObject
 
 from basics.utils import sig_clip
 from basics.bubble_objects import Bubble2D
-from basics.log import blob_log
+from basics.log import blob_log, _prune_blobs
+from basics.bubble_edge import find_bubble_edges
 
 
 eight_conn = np.ones((3, 3))
@@ -186,7 +189,7 @@ class BubbleFinder2D(object):
         return self._bubble_mask
 
     def multiscale_bubblefind(self, scales=None, sigma=None, nsig=2,
-                              overlap_frac=0.8):
+                              overlap_frac=0.8, edge_find=True):
         '''
         Run find_bubbles on the specified scales.
         '''
@@ -198,16 +201,107 @@ class BubbleFinder2D(object):
             sigma = sig_clip(self.array, nsig=10)
 
         self._regions = []
+        all_props = []
+        for i, props in enumerate(blob_log(self.array,
+                                  sigma_list=self.scales,
+                                  overlap=0.99,
+                                  threshold=nsig*sigma,
+                                  weighting=self.weightings)[0][::-1]):
+            # print(i)
+            # Adjust the region properties based on where the bubble edges are
+            if edge_find:
+                coords, shell_fraction, angular_std, mask = \
+                    find_bubble_edges(self.array, props, max_extent=1.25,
+                                      value_thresh=2*sigma,
+                                      nsig_thresh=2, return_mask=True)
+                # find_bubble_edges calculates the shell fraction
+                # If it is below the given fraction, we skip the region.
+                # print(len(coords))
+                if len(coords) < 3:
+                    print("Skipping %s" % (str(i)))
+                    continue
 
-        for props in blob_log(self.array, sigma_list=self.scales,
-                              overlap=overlap_frac,
-                              threshold=nsig*sigma,
-                              weighting=self.weightings)[0]:
+                coords = np.array(coords)
+                ymean = coords[:, 0].mean()
+                xmean = coords[:, 1].mean()
+                coords[:, 0] -= int(ymean)
+                coords[:, 1] -= int(xmean)
+                new_props = np.empty((5,))
+
+                can_fit_ellipse = np.logical_and(shell_fraction > 0.5,
+                                                 angular_std > 0.7)
+
+                if len(coords) > 5 and can_fit_ellipse:
+                    model = ransac(coords[:, ::-1], EllipseModel,
+                                   max(5, int(0.1*len(coords))),
+                                   props[2]/2.)[0]
+                    pars = model.params.copy()
+                    eccent = pars[2] / float(pars[3])
+                    # Sometimes a < b??
+                    if eccent < 1:
+                        eccent = 1. / eccent
+                        pars[2], pars[3] = pars[3], pars[2]
+                        pars[4] = np.angle(np.exp(1j*(pars[4] + 0.5*np.pi)))
+                    if pars[3] < 4.0 or pars[2] > 2.5*props[2] or eccent > 3.:
+                        ellip_fail = True
+                    else:
+                        new_props[0] = pars[1] + int(ymean)
+                        new_props[1] = pars[0] + int(xmean)
+                        new_props[2] = pars[2]
+                        new_props[3] = pars[3]
+                        new_props[4] = pars[4]
+                        ellip_fail = False
+                else:
+                    ellip_fail = True
+
+                if ellip_fail:
+                    model = ransac(coords[:, ::-1], CircleModel,
+                                   max(3, int(0.1*len(coords))),
+                                   props[2]/2.)[0]
+                    if model.params[2] > 2.5*props[2]:
+                        Warning("All fitting failed for: "+str(i))
+                        continue
+                    new_props[0] = model.params[1] + int(ymean)
+                    new_props[1] = model.params[0] + int(xmean)
+                    new_props[2] = model.params[2]
+                    new_props[3] = model.params[2]
+                    new_props[4] = 0.0
+
+            new_coords, new_shell_fraction = \
+                find_bubble_edges(self.array, new_props, max_extent=1.25,
+                                  value_thresh=2*sigma,
+                                  nsig_thresh=2, return_mask=False)[:2]
+            print(shell_fraction)
+            print(new_shell_fraction)
+
+            # print("Shell fraction: ", shell_fraction)
+            # print("Angular std: ", angular_std)
+            # print(new_props)
+            import matplotlib.pyplot as p
+            ax = p.subplot(111)
+            ax.plot(coords[:, 1]+xmean, coords[:, 0]+ymean, 'bo')
+            ax.plot(new_coords[:, 1], new_coords[:, 0], 'go')
+            ax.imshow(self.array, origin='lower', cmap='afmhot')
+            bub = Bubble2D(props)
+            ax.add_patch(bub.as_patch(color='b', fill=False, linewidth=2))
+            bub = Bubble2D(new_props)
+            ax.add_patch(bub.as_patch(color='g', fill=False, linewidth=2))
+            p.draw()
+            import time; time.sleep(0.1)
+            raw_input("?")
+            p.clf()
+
+            new_props = np.append(new_props, shell_fraction)
 
             if self.channel is not None:
-                props = np.append(props, self.channel)
+                new_props = np.append(new_props, self.channel)
 
-            self.regions.append(Bubble2D(props))
+            all_props.append(new_props)
+
+        all_props = _prune_blobs(np.array(all_props), 0.9,
+                                 use_shell_fraction=True)
+
+        self._regions = [Bubble2D(props) for props in all_props]
 
     @property
     def regions(self):

basics/log.py
@@ -105,7 +105,7 @@ def _blob_overlap(blob1, blob2):
     return area / (math.pi * (min(r1, r2) ** 2))
 
 
-def _prune_blobs(blobs_array, overlap):
+def _prune_blobs(blobs_array, overlap, use_shell_fraction=False):
     """Eliminated blobs with area overlap.
 
     Parameters
@@ -126,8 +126,13 @@ def _prune_blobs(blobs_array, overlap):
 
     remove_blobs = []
 
+    if use_shell_fraction:
+        slicer = slice(0, -2)
+    else:
+        slicer = slice(None)
+
     # Create a distance matrix to catch all overlap cases.
-    cond_arr = pdist(blobs_array, metric=partial(overlap_metric))
+    cond_arr = pdist(blobs_array[:, slicer], metric=partial(overlap_metric))
     dist_arr = dist_uppertri(cond_arr, blobs_array.shape[0])
 
     overlaps = np.where(dist_arr >= overlap)
@@ -137,10 +142,20 @@ def _prune_blobs(blobs_array, overlap):
         blob1 = blobs_array[posn1]
         blob2 = blobs_array[posn2]
 
-        if blob1[2] > blob2[2]:
-            remove_blobs.append(posn2)
+        if use_shell_fraction:
+            cond = blob1[5] > blob2[5]
+            if cond:
+                remove_blobs.append(posn2)
+            else:
+                remove_blobs.append(posn1)
+
         else:
-            remove_blobs.append(posn1)
+            cond = blob1[2] > blob2[2]
+            if cond:
+                remove_blobs.append(posn2)
+            else:
+                remove_blobs.append(posn1)
+
 
     # Remove duplicates
     remove_blobs = list(set(remove_blobs))

basics/profile.py
@@ -196,7 +196,7 @@ def radial_profiles(image, blob, ntheta=360, verbose=False,
 
     for i, theta in enumerate(thetas):
 
-        end_pt = (y0 + a*np.cos(theta)*np.sin(pa) - b*np.sin(theta)*np.cos(pa),
+        end_pt = (y0 + a*np.cos(theta)*np.sin(pa) + b*np.sin(theta)*np.cos(pa),
                   x0 + a*np.cos(theta)*np.cos(pa) - b*np.sin(theta)*np.sin(pa))
 
         profile, dists = profile_line(image, (y0, x0), end_pt, **kwargs)

test_wavelet_decomp.py
@@ -25,8 +25,8 @@ sigma = sig_clip(cube[0].value, nsig=10)
 
 bubble_15 = BubbleFinder2D(cube[15], channel=15)
 # bubble_15.apply_bilateral_filter()
-bubble_15.multiscale_bubblefind(sigma=sigma, overlap_frac=0.5)
-bubble_15.region_rejection(value_thresh=3*sigma)
+bubble_15.multiscale_bubblefind(sigma=sigma, overlap_frac=0.75, edge_find=True)
+# bubble_15.region_rejection(value_thresh=3*sigma)
 # bubble_15.apply_atan_transform(np.percentile(bubble_15.array, 90))
 
 cols = ['b', 'g', 'c', 'm', 'r', 'k']
@@ -40,10 +40,10 @@ ax.imshow(bubble_15.array, cmap='afmhot')
 for bub in bubble_15.regions:
     ax.add_patch(bub.as_patch(color='b', fill=False, linewidth=2))
     ax.plot(bub.x, bub.y, 'bD')
-    p.contour(bub.find_shape(cube[15].value, nsig_thresh=1.,
-                             return_array='full',
-                             value_thresh=3*sigma,
-                             linewidth=3), colors='g')
+    # p.contour(bub.find_shape(cube[15].value, nsig_thresh=1.,
+    #                          return_array='full',
+    #                          value_thresh=3*sigma,
+    #                          linewidth=3), colors='g')
 
 p.xlim([0, bubble_15.array.shape[1]])
 p.ylim([0, bubble_15.array.shape[0]])

test_wavelet_decomp_3d.py
@@ -2,6 +2,7 @@
 from spectral_cube import SpectralCube
 import numpy as np
 from astropy.io import fits
+import astropy.units as u
 import os
 import matplotlib.pyplot as p
 from matplotlib.patches import Ellipse
@@ -13,27 +14,22 @@ from basics.utils import sig_clip
 from basics.log import overlap_metric
 
 data_path = "/media/eric/Data_3/VLA/IC1613/"
+# data_path = "/media/eric/MyRAID/M33/14B-088/HI/combined_HI"
 # data_path = "/Users/eric/Data/"
 
 cube = SpectralCube.read(os.path.join(data_path, "IC1613_NA_ICL001.fits"))
+# cube = SpectralCube.read(os.path.join(data_path, "M33_14B-088_AT0206_HI.clean.image.fits"))
 
 # Remove empty channels
 cube = cube[38:63, 500:1500, 500:1500]
+# cube = cube[48:55, 500:1500, 500:1500]
+# cube = cube[:, 960:3400, 1200:2600]
 
 bub_find = BubbleFinder(cube)
 
-bubble_props = bub_find.get_bubbles(verbose=True)
+bubble_props, cluster_idx = \
+    bub_find.get_bubbles(verbose=True, overlap_frac=0.5)
 
-# Now test clustering
-
-# sims = pdist(bubble_props, metric=overlap_metric)
-# sims[sims < 0] = 0.0
-# link_mat = hier.linkage(1 - sims, 'complete')
-# cluster_idx = hier.fcluster(link_mat, 0.9, criterion='distance')
-# radii = np.unique(bubble_props[:, 4])
-
-cluster_idx = hier.fclusterdata(bubble_props[:, 1:3], 18, criterion='distance',
-                                method='complete')
 
 # from sklearn.cluster import DBSCAN
 # cluster_idx = DBSCAN(eps=18, min_samples=3,
@@ -42,6 +38,8 @@ cluster_idx = hier.fclusterdata(bubble_props[:, 1:3], 18, criterion='distance',
 ax = p.subplot(111)
 
 # Show the moment 0
+# mom0 = cube.with_mask(cube>3*bub_find.sigma*u.Jy).moment0()
+# ax.imshow(mom0.value, cmap='afmhot')
 mom0 = fits.getdata(os.path.join(data_path, "IC1613_NA_X0_P_R.fits")).squeeze()
 ax.imshow(mom0[500:1500, 500:1500],
           cmap='afmhot')
@@ -58,14 +56,44 @@ for idx in np.unique(cluster_idx[cluster_idx >= 0]):
     if total > 2:
         # print idx, total
         for blob in bubble_props[cluster_idx == idx]:
-            chan, y, x, rmaj, rmin, pa = blob
+            y, x, rmaj, rmin, pa, chan = blob
             c = Ellipse((x, y), width=2*rmaj, height=2*rmin,
                         angle=np.rad2deg(pa),
                         color=cols[i % len(cols)], fill=False, linewidth=2)
             ax.add_patch(c)
             ax.plot(x, y, cols[i % len(cols)]+'D')
-        x = np.mean(bubble_props[cluster_idx == idx, 2])
-        y = np.mean(bubble_props[cluster_idx == idx, 1])
+        x = np.mean(bubble_props[cluster_idx == idx, 1])
+        y = np.mean(bubble_props[cluster_idx == idx, 0])
+        ax.text(x, y, str(idx), color=cols[i % len(cols)])
+        i += 1
+
+p.xlim([0, cube.shape[2]])
+p.ylim([0, cube.shape[1]])
+
+p.figure()
+
+ax = p.subplot(111)
+ax.imshow(mom0[500:1500, 500:1500],
+          cmap='afmhot')
+# ax.imshow(mom0.value,
+#           cmap='afmhot')
+
+i = 0
+for idx in np.unique(cluster_idx[cluster_idx >= 0]):
+    # if idx in remove_idx:
+    #     continue
+    total = bubble_props[cluster_idx == idx].shape[0]
+    if total <= 2:
+        # print idx, total
+        for blob in bubble_props[cluster_idx == idx]:
+            y, x, rmaj, rmin, pa, chan = blob
+            c = Ellipse((x, y), width=2*rmaj, height=2*rmin,
+                        angle=np.rad2deg(pa),
+                        color=cols[i % len(cols)], fill=False, linewidth=2)
+            ax.add_patch(c)
+            ax.plot(x, y, cols[i % len(cols)]+'D')
+        x = np.mean(bubble_props[cluster_idx == idx, 1])
+        y = np.mean(bubble_props[cluster_idx == idx, 0])
         ax.text(x, y, str(idx), color=cols[i % len(cols)])
         i += 1
 
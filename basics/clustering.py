e09ae9f3a2d2ccb4aeb48016e960a1d5368aa1a8
e-koch <koch.eric.w@gmail.com>
2016-03-30 14:54:00 -0600

    Allow multiprocessing for ellipse overlaps

--- basics/clustering.py
+++ basics/clustering.py
@@ -2,12 +2,22 @@
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
 from scipy.cluster.hierarchy import fcluster, fclusterdata, linkage
+from functools import partial
+
+try:
+    from sklearn.metrics import pairwise_distances
+    from multiprocessing import cpu_count
+    _sklearn_flag = True
+except ImportError:
+    Warning("sklearn must be installed to parallelize distance calculations.")
+    _sklearn_flag = False
 
 from log import overlap_metric
 from utils import mode
 
 
-def cluster_2D_regions(twod_region_props, metric='position', cut_val=18):
+def cluster_2D_regions(twod_region_props, metric='position', cut_val=18,
+                       multiprocess=False, n_jobs=None):
     '''
     Cluster 2D Bubble regions by their postion or overlap.
     '''
@@ -24,7 +34,15 @@ def cluster_2D_regions(twod_region_props, metric='position', cut_val=18):
         if cut_val > 1.0:
             raise ValueError("cut_val <= 1 when metric is overlap.")
 
-        sims = pdist(twod_region_props, metric=overlap_metric)
+        overlap_func = partial(overlap_metric, return_corr=True)
+
+        if multiprocess and _sklearn_flag:
+            if n_jobs is None:
+                n_jobs = cpu_count()
+            sims = pairwise_distances(twod_region_props, metric=overlap_func,
+                                      n_jobs=n_jobs)
+        else:
+            sims = pdist(twod_region_props, metric=overlap_func)
         sims[sims < 0] = 0.0
         link_mat = linkage(1 - sims, 'complete')
         cluster_idx = fcluster(link_mat, cut_val, criterion='distance')
@@ -67,7 +85,11 @@ def cluster_and_clean(twod_region_props, min_scatter=9):
     # Initial cluster is based on position of the centre
     cluster_idx = cluster_2D_regions(twod_region_props,
                                      metric='position',
-                                     cut_val=twod_region_props[:, 3].max())
+                                     cut_val=twod_region_props[:, 2].max())
+
+    # cluster_idx = cluster_2D_regions(twod_region_props,
+    #                                  metric='overlap',
+    #                                  cut_val=0.5, multiprocess=True)
 
     # Finally, we split based on position. At this point, there should be
     # a close cluster of regions and possibly some outliers with small radii
@@ -82,7 +104,8 @@ def cluster_and_clean(twod_region_props, min_scatter=9):
         props = twod_region_props[posns]
 
         # Determine max scatter in cluster from the mode of the major axes.
-        maj_mode = max(mode(props[:, 3]), min_scatter)
+        maj_mode = max(mode(props[:, 2]), min_scatter)
+        # maj_mode = min_scatter
 
         # Cluster on channel and split.
         pos_idx = cluster_2D_regions(props,

9583c4c51c2fd8b0cc3a8e0776af4221883b7e4a
e-koch <koch.eric.w@gmail.com>
2016-04-06 11:43:30 -0600

    Add test for shell similarity

--- basics/tests/test_overlap.py
+++ basics/tests/test_overlap.py
@@ -1,8 +1,10 @@
 
-from basics.log import _ellipse_overlap, _circle_overlap, _min_merge_overlap
+from basics.log import _ellipse_overlap, _circle_overlap, _min_merge_overlap, \
+    shell_similarity
 
 import numpy.testing as npt
 import numpy as np
+import pytest
 
 
 def test_circle_overlap():
@@ -92,3 +94,29 @@ def test_min_merge_overlap():
 
     npt.assert_almost_equal(_min_merge_overlap(F),
                             (2/3.) - np.sqrt(3)/(2*np.pi))
+
+# Enable when packaging is added
+# @pytest.mark.parameterize("pars",
+#                           [((20, 30, 10, 5, 0), (20, 30, 10, 5, 0), 1)
+#                            ])
+def test_shell_similarity():
+
+    t = np.linspace(0, 2 * np.pi, 50)
+
+    test_cases = [((20, 30, 10, 5, 0), (20, 30, 10, 5, 0), 1),
+                  ((20, 30, 10, 5, 0), (20, 30, 9, 5, 0), 1),
+                  ((0, 0, 10, 10, 0), (0, 0, 10, 10, 0), 1),
+                  ((0, 0, 10, 10, 0), (0, 0, 5, 5, 0), 0),
+                  ((0, 0, 10, 10, 0), (5, 5, 5, 5, 0), 0.3),
+                  ((0, 0, 10, 10, 0), (6, 6, 5, 5, 0), 0.24),
+                  ((0, 0, 10, 10, 0), (7, 7, 5, 5, 0), 0.2)]
+
+    def ellip_edges(t, pars):
+        y, x, a, b, pa = pars
+        return np.column_stack([x+a*np.cos(t), y+b*np.sin(t)])
+
+    for params in test_cases:
+        data1 = ellip_edges(t, params[0])
+        data2 = ellip_edges(t, params[1])
+
+        npt.assert_almost_equal(shell_similarity(data1, data2), params[2])

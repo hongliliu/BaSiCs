541078a58d10ad0c9ddf35dbd26330d1d846287a
e-koch <koch.eric.w@gmail.com>
2016-05-05 10:07:58 -0600

    Moved function

--- basics/bubble_segment2D.py
+++ basics/bubble_segment2D.py
@@ -14,8 +14,7 @@ from basics.bubble_objects import Bubble2D
 from basics.log import blob_log, _prune_blobs
 from basics.bubble_edge import find_bubble_edges
 from basics.fit_models import CircleModel, EllipseModel, ransac
-from basics.iterative_watershed import remove_spurs
-from basics.masking_utils import smooth_edges
+from basics.masking_utils import smooth_edges,  remove_spurs
 
 
 eight_conn = np.ones((3, 3))

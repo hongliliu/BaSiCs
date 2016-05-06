90f0dd3389e226c14ab03dc834e559bd3168db59
e-koch <koch.eric.w@gmail.com>
2016-05-06 17:04:13 -0600

    Clean-up old code for bubble objects

--- basics/bubble_objects.py
+++ basics/bubble_objects.py
@@ -1,18 +1,14 @@
 
 import numpy as np
 from astropy.modeling.models import Ellipse2D
-from astropy.nddata.utils import extract_array, add_array
+from astropy.nddata.utils import extract_array
 from astropy.stats import circmean
-from scipy import ndimage as nd
-from itertools import izip
 from spectral_cube.lower_dimensional_structures import LowerDimensionalObject
 from spectral_cube.base_class import BaseNDClass
 from spectral_cube import SpectralCube
 
 from log import overlap_metric
-from utils import consec_split, find_nearest, floor_int, ceil_int, eight_conn,\
-    wrap_to_pi
-from profile import _line_profile_coordinates
+from utils import floor_int, ceil_int, wrap_to_pi
 from fan_pvslice import pv_wedge
 from fit_models import fit_region
 
@@ -72,11 +68,7 @@ class BubbleNDBase(BaseNDClass):
         '''
         Estimate of the perimeter when major ~ minor.
         '''
-        return 2 * np.pi * np.sqrt(0.5*(self.major**2 + self.minor**2))
-
-    @property
-    def extent_mask(self):
-        return self._extent_mask
+        return 2 * np.pi * np.sqrt(0.5 * (self.major**2 + self.minor**2))
 
     @property
     def shell_coords(self):
@@ -91,7 +83,7 @@ class BubbleNDBase(BaseNDClass):
         if x_cent is not None:
             x = x_cent
 
-        return Ellipse((x, y), width=2*rmaj, height=2*rmin,
+        return Ellipse((x, y), width=2 * rmaj, height=2 * rmin,
                        angle=np.rad2deg(pa), **kwargs)
 
     def intensity_props(self, data):
@@ -230,137 +222,10 @@ class Bubble2D(BubbleNDBase):
 
         bbox = self.as_ellipse(zero_center=False).bounding_box
 
-        return array[floor_int(bbox[0][0])-pad:
-                     ceil_int(bbox[0][1])+pad+1,
-                     floor_int(bbox[1][0])-pad:
-                     ceil_int(bbox[1][1])+pad+1]
-
-    def find_extent_mask(self, array, max_extent=1.0,
-                         nsig_thresh=1, value_thresh=None, min_radius_frac=0.5,
-                         radius=None, **kwargs):
-        '''
-        Expand/contract to match the contours in the data.
-
-        Parameters
-        ----------
-        array : 2D numpy.ndarray or spectral_cube.LowerDimensionalObject
-            Data used to define the region boundaries.
-        max_extent : float, optional
-            Multiplied by the major radius to set how far should be searched
-            when searching for the boundary.
-        nsig_thresh : float, optional
-            Number of times sigma above the mean to set the boundary intensity
-            requirement. This is used whenever the local background is higher
-            than the given `value_thresh`.
-        value_thresh : float, optional
-            When given, sets the minimum intensity for defining a bubble edge.
-            The natural choice is a few times the noise level in the cube.
-        min_radius_frac : float, optional
-            Sets a minimum distance to search for the bubble boundary. Defaults
-            to 1/2 of the major radius.
-        radius : float, optional
-            Give an optional radius to use instead of the major radius defined
-            for the bubble.
-        kwargs : passed to profile.profile_line.
-
-        Returns
-        -------
-        extent_mask : np.ndarray
-            Boolean array with the region mask.
-        '''
-
-        mean, std = self.intensity_props(array)
-        background_thresh = mean + nsig_thresh * std
-
-        # Define a suitable background based on the intensity within the
-        # elliptical region
-        if value_thresh is None:
-            value_thresh = background_thresh
-        else:
-            # If value_thresh is higher use it. Otherwise use the bkg.
-            if value_thresh < background_thresh:
-                value_thresh = background_thresh
-
-        # Set the number of theta to be ~ the perimeter.
-        ntheta = 1.5 * ceil_int(self.perimeter)
-
-        # Use the ellipse model to define a bounding box for the mask.
-        bbox = self.as_ellipse(zero_center=True).bounding_box
-
-        y_range = ceil_int((max_extent * 2) *
-                           (bbox[0][1] - bbox[0][0]))
-        x_range = ceil_int((max_extent * 2) *
-                           (bbox[1][1] - bbox[1][0]))
-
-        extent_mask = np.zeros((y_range, x_range), dtype=bool)
-
-        yy, xx = np.mgrid[-int(y_range / 2): int(y_range / 2) + 1,
-                          -int(x_range / 2): int(x_range / 2) + 1]
-
-        dist_arr = np.sqrt(yy**2 + xx**2)
-
-        centre = [arr[0] for arr in np.where(dist_arr == 0.0)]
-
-        for dist, prof, end in self.profile_lines(array,
-                                                  extend_factor=max_extent,
-                                                  append_end=True,
-                                                  ntheta=ntheta, **kwargs):
-
-            new_end = (max_extent * (end[0] - self.y),
-                       max_extent * (end[1] - self.x))
-
-            max_dist = np.abs(dist.max())
-
-            line_posns = []
-            coords = [floor_int(coord)+cent for coord, cent in
-                      zip(_line_profile_coordinates((0, 0), new_end), centre)]
-            for coord in coords:
-                line_posns.append(coord[dist_arr[coords] >=
-                                  min_radius_frac*self.minor])
-
-            prof = prof[dist >= min_radius_frac * self.minor]
-            dist = dist[dist >= min_radius_frac * self.minor]
-
-            above_thresh = np.where(prof >= value_thresh)[0]
-
-            # This angle does not coincide with a shell.
-            # We fill in a pixel at the major radius.
-            if above_thresh.size == 0:
-                nearest_idx = find_nearest(dist_arr[line_posns],
-                                           max_dist).astype(int)
-            else:
-                # Pick the end point on the first segment with > 2 pixels
-                # above the threshold.
-                segments = consec_split(above_thresh)
-
-                for seg in segments:
-                    if seg.size < max(3, int(0.1*above_thresh.size)):
-                        continue
-
-                    # Take the first position and use it to define the edge
-                    dist_val = dist[seg[0]]
-
-                    nearest_idx = \
-                        find_nearest(dist_arr[line_posns],
-                                     dist_val).astype(int)
-                    break
-                else:
-                    # If no segments larger than 2 pixels are found, default
-                    # to the major radius
-                    nearest_idx = \
-                        find_nearest(dist_arr[line_posns],
-                                     max_dist).astype(int)
-
-            end_posn = tuple([posn[:nearest_idx+1] for posn in line_posns])
-
-            extent_mask[end_posn] = True
-
-        # Fill holes
-        extent_mask = nd.binary_fill_holes(extent_mask)
-        extent_mask = nd.binary_closing(extent_mask, eight_conn)
-        extent_mask = nd.binary_opening(extent_mask, eight_conn)
-
-        self._extent_mask = extent_mask
+        return array[floor_int(bbox[0][0]) - pad:
+                     ceil_int(bbox[0][1]) + pad + 1,
+                     floor_int(bbox[1][0]) - pad:
+                     ceil_int(bbox[1][1]) + pad + 1]
 
     def overlap_with(self, other_bubble2D):
         '''
@@ -522,7 +387,7 @@ class Bubble3D(BubbleNDBase):
         '''
 
         try:
-            from pvextractor import Path, extract_pv_slice
+            import pvextractor
         except ImportError:
             raise ImportError("pvextractor must be installed to extract "
                               " PV slices.")
@@ -533,7 +398,7 @@ class Bubble3D(BubbleNDBase):
             spatial_pad = 0
 
         # Define end points along the major axis
-        max_dist = 2*float(self.major + spatial_pad)
+        max_dist = 2 * float(self.major + spatial_pad)
 
         if width is None:
             width = 1  # self.minor
@@ -542,8 +407,8 @@ class Bubble3D(BubbleNDBase):
 
             subcube = self.return_cube_region(cube, **kwargs)
 
-            sub_center = (floor_int(subcube.shape[1]/2.),
-                          floor_int(subcube.shape[2]/2.))
+            sub_center = (floor_int(subcube.shape[1] / 2.),
+                          floor_int(subcube.shape[2] / 2.))
 
             return pv_wedge(subcube, sub_center, max_dist, 0.0, np.pi,
                             width=width)
@@ -564,7 +429,7 @@ class Bubble3D(BubbleNDBase):
             chan_cent = self.channel_center
 
         return Ellipse((x_cent, chan_cent),
-                       width=2*self.major,
+                       width=2 * self.major,
                        height=self.channel_width,
                        angle=0.0, **kwargs)
 
@@ -589,46 +454,6 @@ class Bubble3D(BubbleNDBase):
 
         return ellip_mask
 
-    def find_extent_mask(self, cube, cut_shape=True, **kwargs):
-        '''
-        Run Bubble2D.find_extent_mask to get the extend mask in each channel.
-        '''
-
-        if not self.has_2D_regions:
-            raise NotImplementedError("")
-
-        for i, (chan, region) in enumerate(izip(self._chan_iter(),
-                                                self._twoD_region_iter())):
-
-            if not hasattr(region, "_extent_mask"):
-                region.find_extent_mask(cube[chan], **kwargs)
-
-            # Don't need to add into the full sized array if it already has
-            # the same shape.
-            if region.extent_mask.shape != cube.shape[1:]:
-                extent_2d_mask = np.zeros(cube.shape[1:], dtype=bool)
-                extent_2d_mask = add_array(extent_2d_mask,
-                                           region.extent_mask.copy(),
-                                           region.center_pixel)
-            else:
-                extent_2d_mask = region.extent_mask.copy()
-
-            if i == 0:
-                extent_mask = extent_2d_mask[np.newaxis, :, :]
-            else:
-                extent_mask = np.append(extent_mask,
-                                        extent_2d_mask[np.newaxis, :, :],
-                                        axis=0)
-
-        if cut_shape:
-            slices = nd.find_objects(extent_mask)[0]
-            self._extent_mask = extent_mask[slices]
-            self._extent_offset = (self.channel_start,) + \
-                tuple([pos.start for pos in slices[1:]])
-        else:
-            self._extent_mask = extent_mask
-            self._extent_offset = (self.channel_start, 0, 0)
-
     def as_shell_mask(self, cube):
         pass
 
@@ -640,14 +465,14 @@ class Bubble3D(BubbleNDBase):
         # y, x  extents
         extents = self.find_spatial_extents(zero_center=False)
 
-        spec_slice = slice(max(0, self.channel_start-spec_pad),
+        spec_slice = slice(max(0, self.channel_start - spec_pad),
                            min(cube.shape[0],
-                               self.channel_end+1+spec_pad), 1)
+                               self.channel_end + 1 + spec_pad), 1)
 
-        y_slice = slice(max(0, extents[0]-spatial_pad),
-                        min(cube.shape[1], extents[1]+spatial_pad), 1)
-        x_slice = slice(max(0, extents[2]-spatial_pad),
-                        min(cube.shape[1], extents[3]+spatial_pad), 1)
+        y_slice = slice(max(0, extents[0] - spatial_pad),
+                        min(cube.shape[1], extents[1] + spatial_pad), 1)
+        x_slice = slice(max(0, extents[2] - spatial_pad),
+                        min(cube.shape[1], extents[3] + spatial_pad), 1)
 
         subcube = cube[spec_slice, y_slice, x_slice]
 
@@ -716,8 +541,8 @@ class Bubble3D(BubbleNDBase):
                              interpolation='nearest')
             fig.colorbar(im1, ax=ax1)
 
-            c = self.as_patch(x_cent=floor_int(moment0.shape[1]/2.),
-                              y_cent=floor_int(moment0.shape[0]/2.),
+            c = self.as_patch(x_cent=floor_int(moment0.shape[1] / 2.),
+                              y_cent=floor_int(moment0.shape[0] / 2.),
                               fill=False, color='r', linewidth=2)
             ax1.add_patch(c)
             # ax1.arrow(path_ends[0][0], path_ends[0][1],
@@ -731,11 +556,29 @@ class Bubble3D(BubbleNDBase):
             fig.colorbar(im2, ax=ax2)
 
             c = self.as_pv_patch(fill=False, color='r', linewidth=2,
-                                 chan_cent=pvslice.data.shape[0]/2,
-                                 x_cent=pvslice.data.shape[1]/2)
+                                 chan_cent=pvslice.data.shape[0] / 2,
+                                 x_cent=pvslice.data.shape[1] / 2)
             ax2.add_patch(c)
 
         if return_plot:
             return fig
 
         p.show()
+
+    def __repr__(self):
+        s = "3D Bubble at: ({0:6f}, {1:6f}," \
+            "{2:6f})\n".format(self.channel_center, self.y, self.x)
+        if self.major == self.minor:
+            s += "Radius: {0:6f} \n".format(self.major)
+        else:
+            s += "Major radius: {0:6f} \n".format(self.major)
+            s += "Minor radius: {0:6f} \n".format(self.minor)
+            s += "Position Angle: {0:6f} \n".format(self.pa)
+
+        s += "Channel width: {0:6f} \n".format(self.channel_width)
+        # s += "Spectral width: {0.6f} \n".format(self.velocity_width)
+
+        if self.shell_fraction is not None:
+            s += "Shell fraction: {0:6f} \n".format(self.shell_fraction)
+
+        return s

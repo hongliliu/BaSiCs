e2285a569e8f70e447bf119f46b5bdbefbd7b9c8
e-koch <koch.eric.w@gmail.com>
2016-05-19 16:18:08 -0600

    Updated 3D test script

--- test_wavelet_decomp_3d.py
+++ test_wavelet_decomp_3d.py
@@ -1,115 +1,34 @@
 
 from spectral_cube import SpectralCube
-import numpy as np
-from astropy.io import fits
 import astropy.units as u
+from astropy.io import fits
 import os
-import matplotlib.pyplot as p
-from matplotlib.patches import Ellipse
-from scipy.spatial.distance import pdist, squareform
-import scipy.cluster.hierarchy as hier
 
 from basics.bubble_segment3D import BubbleFinder
-from basics.utils import sig_clip
-from basics.log import overlap_metric
 
-data_path = "/media/eric/Data_3/VLA/IC1613/"
-# data_path = "/media/eric/MyRAID/M33/14B-088/HI/combined_HI"
+data_path = "/media/eric/Data_3/LITTLE_THINGS/IC1613/"
+# data_path = "/media/eric/Data_3/LITTLE_THINGS/IC10/"
+# data_path = "/home/ekoch/Data/IC10/"
+# data_path = "/home/ekoch/Data/IC1613/"
 # data_path = "/Users/eric/Data/"
+# data_path = "/media/eric/Data_3/VLA/THINGS/HO_II/"
 
 cube = SpectralCube.read(os.path.join(data_path, "IC1613_NA_ICL001.fits"))
-# cube = SpectralCube.read(os.path.join(data_path, "M33_14B-088_AT0206_HI.clean.image.fits"))
+# cube = SpectralCube.read(os.path.join(data_path, "IC10_NA_ICL001.fits"))
+# cube = SpectralCube.read(os.path.join(data_path, "HO_II_NA_CUBE_THINGS.FITS"))
 
 # Remove empty channels
-cube = cube[38:63, 500:1500, 500:1500]
-# cube = cube[48:55, 500:1500, 500:1500]
-# cube = cube[:, 960:3400, 1200:2600]
+cube = cube[:, 500:1500, 500:1500]
 
-bub_find = BubbleFinder(cube)
+bub_find = BubbleFinder(cube, keep_threshold_mask=True)
 
-bubble_props, cluster_idx = \
-    bub_find.get_bubbles(verbose=True, overlap_frac=0.5)
-
-
-# from sklearn.cluster import DBSCAN
-# cluster_idx = DBSCAN(eps=18, min_samples=3,
-#                      metric='euclidean').fit(bubble_props[:, 1:3]).labels_
-
-ax = p.subplot(111)
+bub_find.get_bubbles(verbose=True, overlap_frac=0.5, multiprocess=True,
+                     refit=False, nsig=2.0, cut_val=0.5, min_channels=5,
+                     distance=0.84 * u.Mpc, nprocesses=None)
 
 # Show the moment 0
-# mom0 = cube.with_mask(cube>3*bub_find.sigma*u.Jy).moment0()
-# ax.imshow(mom0.value, cmap='afmhot')
 mom0 = fits.getdata(os.path.join(data_path, "IC1613_NA_X0_P_R.fits")).squeeze()
-ax.imshow(mom0[500:1500, 500:1500],
-          cmap='afmhot')
+# mom0 = fits.getdata(os.path.join(data_path, "IC10_NA_X0_P_R.fits")).squeeze()
+# mom0 = fits.getdata(os.path.join(data_path, "HO_II_NA_MOM0_THINGS.FITS")).squeeze()
 
-# ax.imshow(cube[10].value, cmap='afmhot')
-
-cols = ['b', 'g', 'c', 'm', 'r', 'y']
-
-i = 0
-for idx in np.unique(cluster_idx[cluster_idx >= 0]):
-    props = bubble_props[cluster_idx == idx]
-    total = 1 + props[:, -1].max() - props[:, -1].min()
-    if total > 2:
-        for blob in props:
-            y, x, rmaj, rmin, pa, chan = blob
-            c = Ellipse((x, y), width=2*rmaj, height=2*rmin,
-                        angle=np.rad2deg(pa),
-                        color=cols[i % len(cols)], fill=False, linewidth=2)
-            ax.add_patch(c)
-            ax.plot(x, y, cols[i % len(cols)]+'D')
-        x = np.mean(bubble_props[cluster_idx == idx, 1])
-        y = np.mean(bubble_props[cluster_idx == idx, 0])
-        ax.text(x, y, str(idx), color=cols[i % len(cols)])
-        i += 1
-
-p.xlim([0, cube.shape[2]])
-p.ylim([0, cube.shape[1]])
-
-p.figure()
-
-ax = p.subplot(111)
-ax.imshow(mom0[500:1500, 500:1500],
-          cmap='afmhot')
-# ax.imshow(mom0.value,
-#           cmap='afmhot')
-
-i = 0
-for idx in np.unique(cluster_idx[cluster_idx >= 0]):
-    props = bubble_props[cluster_idx == idx]
-    total = 1 + props[:, -1].max() - props[:, -1].min()
-    if total <= 2:
-        for blob in props:
-            y, x, rmaj, rmin, pa, chan = blob
-            c = Ellipse((x, y), width=2*rmaj, height=2*rmin,
-                        angle=np.rad2deg(pa),
-                        color=cols[i % len(cols)], fill=False, linewidth=2)
-            ax.add_patch(c)
-            ax.plot(x, y, cols[i % len(cols)]+'D')
-        x = np.mean(bubble_props[cluster_idx == idx, 1])
-        y = np.mean(bubble_props[cluster_idx == idx, 0])
-        ax.text(x, y, str(idx), color=cols[i % len(cols)])
-        i += 1
-
-p.xlim([0, cube.shape[2]])
-p.ylim([0, cube.shape[1]])
-
-# num_channels = (test_cube > 0).sum(0)
-# clean_cube = (test_cube > 0).astype(int)
-
-# forward = np.roll(clean_cube, 1, axis=0) - clean_cube
-# backward = np.roll(clean_cube, -1, axis=0) - clean_cube
-
-# extents = np.logical_or(forward == -1, backward == -1)
-
-# has_one = np.any(extents, axis=0)
-
-# for y, x in izip(*np.where(has_one)):
-#     chan_diff = np.diff(np.where(extents[:, y, x]))
-#     if not np.any(chan_diff):
-#         # Remove that object
-#         chan = np.where(extents[:, y, x])[0][0]
-#         lab = test_cube[chan, y, x]
-#         clean_cube[chan, np.where(test_cube[chan] == lab)] = 0
+bub_find.visualize_bubbles(moment0=mom0[500:1500, 500:1500])

a6491e62201e070665020e8e123d1cd65fc2cca6
e-koch <koch.eric.w@gmail.com>
2016-05-30 11:05:18 -0600

    Write the error and output files with the galaxy name and in the right folder

--- Examples/THINGS/submit_all_THINGS.py
+++ Examples/THINGS/submit_all_THINGS.py
@@ -1,9 +1,14 @@
 
 import os
+from datetime import datetime
 
 '''
 Submits a job for every sample defined in the info dict
 '''
+
+
+def timestring():
+    return datetime.now().strftime("%Y%m%d%H%M%S%f")
 
 script_path = "/lustre/home/ekoch/code_repos/BaSiCs/Examples/THINGS/"
 
@@ -16,5 +21,13 @@ datapath = "/lustre/home/ekoch/THINGS/"
 
 for name in galaxy_props:
     galaxy_path = os.path.join(datapath, name)
+    now_time = timestring()
+    error_file = \
+        os.path.join(galaxy_path, "{0}_bubbles_{1}.err".format(name, now_time))
+    output_file = \
+        os.path.join(galaxy_path, "{0}_bubbles_{1}.out".format(name, now_time))
     # Now submit it!
-    os.system("qsub -v INP={1} {0}".format(submit_file, galaxy_path))
+    os.system("qsub -e {2} -o {3} -v INP={1} {0}".format(submit_file,
+                                                         galaxy_path,
+                                                         error_file,
+                                                         output_file))
